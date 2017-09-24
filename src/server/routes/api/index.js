d21526c065fcc4a6394b4280207e8d40aa28f5b0
Daniel Dyla <dyladan@gmail.com>
2017-09-24 15:19:13 -0400

    Mongo and very basic user auth

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -1,9 +1,20 @@
 "use strict";
 
 const router = require("express").Router();
+const Users = require("../../models/users");
+const passport = require("passport");
 
 router.get("/", (req, res) => {
   res.status(200).send("API call");
+});
+
+router.get("/users", async (req, res) => {
+  if (req.isAuthenticated()) {
+    const users = await Users.find();
+    res.json(users);
+  } else {
+    res.status(401).send();
+  }
 });
 
 module.exports = router;
--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,19 @@
 "use strict";
 
 const router = require("express").Router();
+const passport = require("passport");
 
 router.get("/", (req, res) => {
   res.status(200).send("Hello World!");
+});
+
+router.post("/login", passport.authenticate("local"), (req, res) => {
+  res.json({ success: true, name: req.user.name });
+});
+
+router.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.json({ success: true });
 });
 
 router.use("/api/v1", require("./api"));
