afe742a3501cb4470e144c11eb1af4c2de350b09
Chase699 <chaseehlert123@gmail.com>
2017-09-27 10:27:49 -0400

    Working on implementing start screen buttons.

---
+++ src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/StartScreenActivity.java
@@ -0,0 +1,27 @@
+package com.example.chase.dontpaniceducational;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+
+    public void signUp(View view) {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+
+    }
+
+    public void aboutUs(View view) {
+
+    }
+}
