d4ac92a4480f0e8ee0fa25ef3e9c8e405b8f6ad9
Chase <chaseehlert123@gmail.com>
2017-10-03 19:23:52 -0400

    Changed the app's color theme to what the team designated, finished functionality of the text fields and buttons in the activities directly linked to the start screen activity.

--- src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/SignUpActivity.java
+++ src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/SignUpActivity.java
@@ -1,31 +1,40 @@
 package com.example.chase.dontpaniceducational;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+    private EditText signUpFirstName, signUpLastName, signUpEmail, signUpPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
 
-    EditText signUpFirstName = (EditText) findViewById(R.id.editText_firstNameSignUp);
-    EditText signUpLastName = (EditText) findViewById(R.id.editText_lastNameSignUp);
-    EditText signUpEmail = (EditText) findViewById(R.id.editText_emailAddressSignUp);
-    EditText signUpPassword = (EditText) findViewById(R.id.editText_passwordSignUp);
+        signUpFirstName = (EditText) findViewById(R.id.editText_firstNameSignUp);
+        signUpLastName = (EditText) findViewById(R.id.editText_lastNameSignUp);
+        signUpEmail = (EditText) findViewById(R.id.editText_emailAddressSignUp);
+        signUpPassword = (EditText) findViewById(R.id.editText_passwordSignUp);
+    }
 
     public void userInfo(View view) {
-        String stringFirstName = signUpFirstName.toString();
-        String stringLastName = signUpLastName.toString();
-        String stringEmailAddress = signUpEmail.toString();
-        String stringPassword = signUpPassword.toString();
+        String stringFirstName = signUpFirstName.getText().toString();
+        String stringLastName = signUpLastName.getText().toString();
+        String stringEmailAddress = signUpEmail.getText().toString();
+        String stringPassword = signUpPassword.getText().toString();
+        if (stringFirstName.matches("") || stringLastName.matches("") || stringEmailAddress.matches("") || stringPassword.matches("")) {
+            Toast.makeText(this, "You cannot continue because one of the fields is empty.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent intent = new Intent(this, PanicActivity.class);
+        startActivity(intent);
     }
 
-    public void clearFields(View view) {
+    public void clearFieldsSignUp(View view) {
         signUpFirstName.setText("");
         signUpLastName.setText("");
         signUpEmail.setText("");
