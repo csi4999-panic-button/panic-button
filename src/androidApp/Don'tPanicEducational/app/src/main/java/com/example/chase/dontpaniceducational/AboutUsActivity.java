958dab88e7fc1effd7d95044c003e44094b19e33
Chase699 <chaseehlert123@gmail.com>
2017-10-03 14:37:30 -0400

    Machine switch

--- src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/AboutUsActivity.java
+++ src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/AboutUsActivity.java
@@ -1,7 +1,9 @@
 package com.example.chase.dontpaniceducational;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AboutUsActivity extends AppCompatActivity {
 
--- src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/LoginActivity.java
+++ src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.chase.dontpaniceducational;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void cancelButtonClick(View view) {
+        finish();
+    }
+
+    public void loginButtonClick(View view) {
+        EditText loginUsername = (EditText) findViewById(R.id.editText_login);
+        EditText loginPassword = (EditText) findViewById(R.id.editText_passwordLogin);
+        String username = loginUsername.toString();
+        String password = loginPassword.toString();
     }
 }
