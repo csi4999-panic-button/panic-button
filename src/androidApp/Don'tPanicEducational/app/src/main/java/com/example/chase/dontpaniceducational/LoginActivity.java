eb1bc6a6b7c8483e03c494475e24e3c929f2bf46
Chase <chaseehlert123@gmail.com>
2017-10-03 17:01:51 -0400

    Machine Switch

--- src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/LoginActivity.java
+++ src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.chase.dontpaniceducational;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
--- src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/SignUpActivity.java
+++ src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/SignUpActivity.java
@@ -2,6 +2,8 @@ package com.example.chase.dontpaniceducational;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    EditText signUpFirstName = (EditText) findViewById(R.id.editText_firstNameSignUp);
+    EditText signUpLastName = (EditText) findViewById(R.id.editText_lastNameSignUp);
+    EditText signUpEmail = (EditText) findViewById(R.id.editText_emailAddressSignUp);
+    EditText signUpPassword = (EditText) findViewById(R.id.editText_passwordSignUp);
+
+    public void userInfo(View view) {
+        String stringFirstName = signUpFirstName.toString();
+        String stringLastName = signUpLastName.toString();
+        String stringEmailAddress = signUpEmail.toString();
+        String stringPassword = signUpPassword.toString();
+    }
+
+    public void clearFields(View view) {
+        signUpFirstName.setText("");
+        signUpLastName.setText("");
+        signUpEmail.setText("");
+        signUpPassword.setText("");
     }
 }
