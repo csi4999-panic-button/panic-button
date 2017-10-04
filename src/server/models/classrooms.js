45e68eb71fe796bb2d8f7a8ebed3f67da60d0fbe
tcarrio <tom@carrio.me>
2017-10-04 16:42:32 -0400

    Added separate invite code schema

--- src/server/models/classrooms.js
+++ src/server/models/classrooms.js
@@ -1,43 +1,19 @@
 "use strict";
 
 const mongoose = require("mongoose");
-const crypto = require("crypto");
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, lowercase: true, index: { unique: true } },
-  password: { type: String, required: true, select: false },
-  name: { type: String, required: true },
-  apiToken: { type: String, default: getToken, unique: true, select: false },
-}, {
-  timestamps: true,
-});
-
-// invite codes need to be unique across all of them
-// how to set unique for all of them?
-// consider different length codes and requirements for string length?
 const classroomSchema = new mongoose.Schema({
     id: { type: String, required: true, lowercase: true, index: { unique: true} },
-    course_type: { type: String, required: true, lowercase: true },
-    course_number: { type: String, required: true, lowercase: true },
-    section_number: { type: String, required: false, lowercase: true },
-    teachers: {
-        ids: [String],
-        code: {type: String, required: false, lowercase: true }
-    },
-    teacher_assistants: {
-        ids: [String],
-        code: {type: String, required: false, lowercase: true }
-    },
-    students: {
-        ids: [String],
-        code: {type: String, required: true, lowercase: true }
-    }
+    schoolName: { type: String, required: false },
+    courseType: { type: String, required: false, lowercase: true },
+    courseNumber: { type: String, required: false, lowercase: true },
+    sectionNumber: { type: String, required: false, lowercase: true },
+    courseTitle: { type: String, required: true },
+    teachers: { type: [String], required: true, index: true },
+    teacherAssistants: { type: [String], required: false, index: true },
+    students: { type: [String], required: false, index: true }
 }, {
     timestamps: true,
 })
 
-function getCode() {
-    const buffer = crypto.randomBytes(24);
-    const code = buffer.toString('hex');
-    return code;
-}
+module.exports = mongoose.model("Classroom", classroomSchema);
