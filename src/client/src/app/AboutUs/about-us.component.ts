6fe10ff824ec2b3ff287d0ad325c88bd73b0e398
liPmatt <limatt8791@gmail.com>
2017-10-10 07:31:45 -0400

    Login, Signin, and About Pages Ported Over

---
+++ src/client/src/app/AboutUs/about-us.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-us',
+  templateUrl: './about-us.html',
+  styleUrls: ['./about-us.css']
+})
+export class AboutUsComponent { }
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -34,19 +34,26 @@
 // })
 // export class AppModule { }
 
-import { NgModule }             from '@angular/core';
-import { BrowserModule }        from '@angular/platform-browser';
-import { FormsModule }          from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { MdButtonModule } from '@angular/material';
+import {MatInputModule} from '@angular/material';
 
-import { AppComponent }          from './app.component';
-import { CrisisListComponent }   from './crisis-list.component';
-import { HomePageComponent }     from './home-page.component';
-import { PageNotFoundComponent }     from './not-found.component';
+import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list.component';
+import { HomePageComponent } from './home-page.component';
+import { PageNotFoundComponent } from './not-found.component';
+import { LoginPageComponent } from './LoginPage/login-page.component';
+import { AboutUsComponent } from './AboutUs/about-us.component';
+import { SignUpComponent } from './SignUp/sign-up.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomePageComponent },
+  { path: 'login-page', component: LoginPageComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'sign-up', component: SignUpComponent },
   { path: 'crisis-center', component: CrisisListComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
@@ -58,6 +65,7 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     MdButtonModule,
+    MatInputModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
@@ -67,6 +75,9 @@ const appRoutes: Routes = [
     AppComponent,
     HomePageComponent,
     CrisisListComponent,
+    LoginPageComponent,
+    AboutUsComponent,
+    SignUpComponent,
     PageNotFoundComponent
   ],
   bootstrap: [ AppComponent ]
