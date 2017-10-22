119a6b55ac291f186f169dfef54f3d17800dc2fa
Chase <chaseehlert123@gmail.com>
2017-10-22 14:35:39 -0400

    Machine switch

--- src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/SendLoginCredentials.java
+++ src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/SendLoginCredentials.java
@@ -1,7 +1,11 @@
 package com.example.chase.dontpaniceducational;
 
 import android.os.AsyncTask;
+import android.util.JsonReader;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -10,6 +14,14 @@ public abstract class SendLoginCredentials extends AsyncTask<URL, Integer, Long>
         try {
             URL apiEndpoint = new URL("http://www.panic-button.stream");
             HttpURLConnection myConnection = (HttpURLConnection) apiEndpoint.openConnection();
+            myConnection.setRequestProperty("User-Agent", "android-app");
+            if (myConnection.getResponseCode() == 200) {
+                InputStream responseBody = myConnection.getInputStream();
+                InputStreamReader responeBodyReader = new InputStreamReader(responseBody, "UTF-8");
+                JsonReader jsonReader = new JsonReader(responeBodyReader);
+            } else {
+
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
