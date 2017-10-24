033b7321968303cdd9bb6f11ca7383cb4f97c804
tcarrio <tom@carrio.me>
2017-10-24 13:17:13 -0400

    Updated classroom routes to include invite code logic

--- src/server/routes/api/classrooms/index.js
+++ src/server/routes/api/classrooms/index.js
@@ -6,19 +6,17 @@ const InviteCodes = require("../../../models/invite-codes");
 
 // returns a list of classrooms this user belongs to
 router.get("/", async (req, res) => {
-  if (req.isAuthenticated()) {
-    Classrooms.find({ $or: [
-      { teachers: req.user._id },
-      { teacherAssistants: req.user._id },
-      { students: req.user._id }
-    ]}).then(classrooms => {
-      res.json(classrooms);
-    }).catch( err => {
-      res.json({ status: false, message: err });
-    });
-  } else {
+  if (!req.isAuthenticated()) {
     res.status(401).send();
   }
+
+  return Classrooms.find({ $or: [
+    { teachers: req.user._id },
+    { teacherAssistants: req.user._id },
+    { students: req.user._id }
+  ]})
+  .then(classrooms => res.json(classrooms))
+  .catch( err => res.json({ status: false, message: err }));
 });
 
 // creates a classroom and returns invite codes for teachers, TAs, and students
@@ -49,68 +47,116 @@ router.post("/", async (req, res) => {
 
 // updates a classroom document by classroomId
 router.put("/id/:classroomId", async (req, res) => {
-  if (req.isAuthenticated()) {
-    Classrooms.findOneAndUpdate(
-        { $and: [ { id: req.params.classroomId } , { teachers: req.user._id } ] },
-        { $set: req.body }
-        ).then( classroom => {
-      console.log(classroom);
-      if( classroom !== null ){
-        res.json({status: true, message: "Class was successfully updated", classroom: classroom });
-      } else {
-        res.json({status: false, message: "Cannot find/modify class" });
-      }
-    }).catch( err => {
-          res.json({ status: false, message: err });
-        });
-  } else {
+  if (!req.isAuthenticated()) {
     res.status(401).send();
   }
+
+  return Classrooms.findOneAndUpdate(
+    { id: req.params.classroomId, teachers: req.user._id },
+    { $set: req.body } // THIS NEEDS TO BE SANITIZED
+  ).then( (classroom) => {
+    if( classroom !== null ){
+      return res.json({status: true, message: "Class was successfully updated", classroom: classroom });
+    }
+    
+    return res.json({status: false, message: "Cannot find/modify class" });
+  }).catch( err => res.json({ status: false, message: err.message }))
 });
 
 // adds user to the listing for a classroom based on a given invite code
 router.post("/join", async (req, res) => {
-  if (req.isAuthenticated()) {
-    const qCode = req.body.inviteCode
-      InviteCodes.findOne(
-          { code: qCode }
-          ).then( validCode => {
-        var listUpdate = {};
-        switch(validCode.type) {
-          case 1:
-            listUpdate = { teachers: req.user._id }
-            break;
-          case 2:
-            listUpdate = { teacherAssistants: req.user._id }
-            break;
-          case 3:
-            listUpdate = { students: req.user._id }
-            break;
-          default:
-            throw Error("No matching code type for " + validCode);
-        }
-        Classrooms.update(
-            { _id: validCode.classroom },
-            { $addToSet: listUpdate },
-            { upsert: false }
-            ).then( classUpdate => {
-          console.log(classUpdate);
-          if(classUpdate.ok === 1){
-            res.json({ success: true, message: "You successfully belong to the classroom" });
-          } else {
-            res.json({ success: false, message: "You could not be added to that classroom" });
-          }
-        }).catch( err => {
-              res.json({ success: false, message: err });
-            });
+  if (!req.isAuthenticated()) {
+    return res.status(401).send();
+  }
+  
+  const qCode = req.body.inviteCode
+  InviteCodes.findOne({ code: qCode })
+  .then( validCode => {
+    if(validCode.length === 0){
+      throw new Error("That invite code could not be found");
+    }
 
-      }).catch( err => {
-        res.json({success: false, message: err });
-      });
-  } else {
+    var listUpdate = {};
+    switch(validCode.type) {
+      case 1:
+        listUpdate = { teachers: req.user._id }
+        break;
+      case 2:
+        listUpdate = { teacherAssistants: req.user._id }
+        break;
+      case 3:
+        listUpdate = { students: req.user._id }
+        break;
+      default:
+        throw Error("No matching code type for " + validCode);
+    }
+
+    Classrooms.update(
+      { _id: validCode.classroom },
+      { $addToSet: listUpdate },
+      { upsert: false }
+    ).then( classUpdate => {
+      console.log(classUpdate);
+      if(classUpdate.ok === 1){
+        return res.json({ success: true, message: "You successfully belong to the classroom" });
+      }
+
+      return res.json({ success: false, message: "You could not be added to that classroom" });
+
+    }).catch( err => res.json({success: false, message: err.message }));
+  }).catch( err => res.json({success: false, message: err.message }));
+});
+
+router.put("/:classroomId/code/:type", (req,res) => {
+  if (!req.isAuthenticated()) {
     res.status(401).send();
   }
+
+  Classrooms.find({$and: [
+      { _id: req.params.classroomId },
+      { teachers: req.user._id }
+  ]}).then( (classroom) => {
+    if(classroom.length !== 1){
+      throw new Error("This code did not match a classroom you teach");
+    }
+
+    return InviteCodes.find({ 
+      classroom: req.params.classroomId,
+      type: req.params.type
+    });
+  }).then( (inviteCode) => {
+    inviteCode.rotateCode();
+    res.json({
+        success: true, 
+        inviteCode: inviteCode.code,
+        type: util.getInviteType(inviteCode.type)
+    });
+  }).catch( err => res.json({success: false, message: err.message }));
 });
+
+router.delete("/:classroomId/code/:code", (req, res) => {
+  if(!req.isAuthenticated()){
+    return res.status(401).send();
+  }
+
+  Classrooms.findOne({ 
+    _id: inviteCode.classroom ,
+    teachers: req.user._id 
+  }).then( classroom => {
+    if( classroom == null ){
+      throw new Error("This invite code does not match a classroom you teach");
+    }
+    return InviteCodes.findOneAndRemove({code: req.params.code })
+  }).then( inviteCode => {
+    console.log(inviteCode);
+    if( inviteCode == null){
+      return res.json({ success: false, message: "This invite code does not exist" });
+    }
+
+    return res.json({ success: true, message: "This invite code was removed" });
+  }).catch( err => res.json({success: false, message: err.message }));
+});
+
 
 module.exports = router;
 
