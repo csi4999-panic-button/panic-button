5d656436c90b2386c1452c5e4c970bfd650494d7
liPmatt <limatt8791@gmail.com>
2017-10-26 20:46:41 -0400

    UI Changes

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -39,22 +39,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { MdButtonModule } from '@angular/material';
-import {MatInputModule} from '@angular/material';
+import { MatInputModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { CrisisListComponent } from './crisis-list.component';
 import { HomePageComponent } from './home-page.component';
 import { PageNotFoundComponent } from './not-found.component';
 import { LoginPageComponent } from './LoginPage/login-page.component';
 import { AboutUsComponent } from './AboutUs/about-us.component';
 import { SignUpComponent } from './SignUp/sign-up.component';
+import { UserConsoleComponent} from './UserConsole/user-console.component';
+import { ClassHubComponent } from './ClassHub/class-hub.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'login-page', component: LoginPageComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'sign-up', component: SignUpComponent },
-  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'user-console', component: UserConsoleComponent },
+  { path: 'class-hub', component: ClassHubComponent },
   { path: '',   redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
   
@@ -74,10 +76,11 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HomePageComponent,
-    CrisisListComponent,
     LoginPageComponent,
     AboutUsComponent,
     SignUpComponent,
+    UserConsoleComponent,
+    ClassHubComponent,
     PageNotFoundComponent
   ],
   bootstrap: [ AppComponent ]
