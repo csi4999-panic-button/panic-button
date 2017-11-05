be08ea0eea0864e3f4cdae9c3aaf968ba3104413
liPmatt <limatt8791@gmail.com>
2017-11-05 13:58:45 -0500

    api test front end

--- src/client/src/app/LoginPage/login-page.component.ts
+++ src/client/src/app/LoginPage/login-page.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'login-page',
   templateUrl: './login-page.html',
-  styleUrls: ['./login-page.css']
+  styleUrls: ['./login-page.css'],
 })
-export class LoginPageComponent { }
+
+export class LoginPageComponent{
+  protected userData: any;
+  protected userName: string;
+  protected password: any;
+  protected flag: string;
+  protected body: any;
+
+  constructor(private http: HttpClient) {
+    const url = 'http://www.panic-button.stream/login';
+    http.post(url, {email:"dandyla@oakland.edu", password:"testpass"})
+    .subscribe();
+
+    if(this.userData == null)
+    {
+      this.flag = "no data";
+    }
+    else
+    {
+      this.flag = "got data";
+    }
+  }
+ }
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,45 +1,11 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
-// import { MdButtonModule } from '@angular/material';
-// import { FormsModule } from '@angular/forms';
-// import { AppComponent } from './app.component';
-// // import { AppRoutingModule } from './app-routing-module';
-// import { LoginPageComponent } from './LoginPage/login-page-component'
-// import { RouterModule, Routes } from '@angular/router';
-
-// const appRoutes: Routes = [
-//   //{ path: '',   redirectTo: '/login-page', pathMatch: 'full' },
-//     { path: 'app-root', component:  AppComponent},
-//     { path: 'login-page', component: LoginPageComponent},
-//     {
-//       path: 'home',
-//       component: AppComponent,
-//     },
-//     { path: '',
-//     redirectTo: '/home',
-//     pathMatch: 'full'}
-// ];
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     LoginPageComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     MdButtonModule
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { MdButtonModule } from '@angular/material';
 import { MatInputModule} from '@angular/material';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page.component';
@@ -49,6 +15,7 @@ import { AboutUsComponent } from './AboutUs/about-us.component';
 import { SignUpComponent } from './SignUp/sign-up.component';
 import { UserConsoleComponent} from './UserConsole/user-console.component';
 import { ClassHubComponent } from './ClassHub/class-hub.component';
+
 
 const appRoutes: Routes = [
   { path: 'home', component: HomePageComponent },
@@ -71,7 +38,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
-    )
+    ),
+    HttpClientModule
   ],
   declarations: [
     AppComponent,
