f02c09ab1b271230e266ed2cfb2bef9c80f9aaa5
liPmatt <limatt8791@gmail.com>
2017-11-05 14:01:32 -0500

    minor changes

--- src/client/src/app/LoginPage/login-page.component.ts
+++ src/client/src/app/LoginPage/login-page.component.ts
@@ -11,21 +11,11 @@ export class LoginPageComponent{
   protected userData: any;
   protected userName: string;
   protected password: any;
-  protected flag: string;
   protected body: any;
 
   constructor(private http: HttpClient) {
     const url = 'http://www.panic-button.stream/login';
     http.post(url, {email:"dandyla@oakland.edu", password:"testpass"})
     .subscribe();
-
-    if(this.userData == null)
-    {
-      this.flag = "no data";
-    }
-    else
-    {
-      this.flag = "got data";
-    }
   }
  }
