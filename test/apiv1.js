ec3d96dad25162329583d73d81d0fa40fc2e1637
tcarrio <tom@carrio.me>
2017-11-05 20:43:39 -0500

    Latest tests covering root, api/v1, and utils

--- test/apiv1.js
+++ test/apiv1.js
@@ -1,11 +1,73 @@
 "use strict";
 
-const request = require("request-promise");
+const request = require("request-promise").defaults({jar: true});
+const tough = require("tough-cookie");
 const baseUrl = "http://localhost:3000"
+const users = require("./data/users");
 
-describe("Root", () => {
+
+// This series of tests uses user[0] for registration. login, and manipulation
+describe("API v1", () => {
     it("should return a string 'API call'", async () => {
         const apiCallStr = await request.get(baseUrl+"/api/v1");
         expect(apiCallStr).to.equal("API call");
+    });
+
+    it("should return Unauthorized with no session created", async () => {
+        try{
+            const unauthReq = await request.get(baseUrl + "/api/v1/users");
+            expect(unauthReq.statusCode).to.equal(401);
+        } catch(err) {
+            expect(err.name).to.equal("StatusCodeError");
+            expect(err.statusCode).to.equal(401);
+        }
+    });
+
+    it("should return the users API token", async () => {
+        const thisUser = users[0];
+        const userOpts = {
+            method: 'POST',
+            uri: baseUrl + "/register",
+            json: true,
+            body: thisUser,
+        };
+        const loginOpts = {
+            method: 'POST',
+            uri: baseUrl + "/login",
+            json: true,
+            body: {
+                email: thisUser.email,
+                password: thisUser.password
+            }
+        };
+        const tokenOpts = {
+            method: 'POST',
+            uri: baseUrl + "/api/v1/authenticate",
+            json: true,
+        };
+
+        // register the new user account
+        const register = await request(userOpts);
+        expect(register.firstName).to.equal(thisUser.firstName);
+
+        // login to the new user account
+        const login = await request(loginOpts);
+        expect(login.success).to.equal(true);
+
+        // request apiToken with cookie set
+        const userToken = await request(tokenOpts);
+        expect(userToken.token).to.equal(register.apiToken);
+    });
+
+    it("should list all users in the database", async () => {
+        const thisUser = users[0];
+        const reqOpts = {
+            method: 'GET',
+            uri: baseUrl + "/api/v1/users",
+            json: true,
+        }
+        
+        const panicUsers = await request(reqOpts);
+        expect(panicUsers[0].firstName).to.equal(thisUser.firstName);
     });
 })
---
+++ test/base.js
@@ -0,0 +1,60 @@
+"use strict";
+
+const request = require("request-promise");
+const baseUrl = "http://localhost:3000"
+const users = require("./data/users");
+
+// This series of tests uses user[1] for registration. login, and manipulation
+describe("User Access", () => {
+    it("should successfully register a new user", async () => {
+        const thisUser = users[1];
+        const userOpts = {
+            method: 'POST',
+            uri: baseUrl + "/register",
+            json: true,
+            body: thisUser
+        };
+        
+        const register = await request(userOpts);
+        expect(register.firstName).to.equal(thisUser.firstName);
+    });
+
+    it("should successfully login as the new user", async () => {
+        const thisUser = users[1];
+        const loginOpts = {
+            method: 'POST',
+            uri: baseUrl + "/login",
+            json: true,
+            body: {
+                email: thisUser.email,
+                password: thisUser.password
+            }
+        };
+
+        const login = await request(loginOpts);
+        expect(login.success).to.equal(true);
+    });
+
+    it("should log out the user from their session", async () => {
+        const thisUser = users[1];
+        const loginOpts = {
+            method: 'POST',
+            uri: baseUrl + "/login",
+            json: true,
+            body: {
+                email: thisUser.email,
+                password: thisUser.password
+            }
+        };
+        const logoutOpts = {
+            method: 'GET',
+            uri: baseUrl + "/logout",
+            json: true
+        }
+
+        const login = await request(loginOpts);
+        expect(login.success).to.equal(true);
+        const logout = await request(logoutOpts);
+        expect(logout.success).to.equal(true);
+    });
+})
