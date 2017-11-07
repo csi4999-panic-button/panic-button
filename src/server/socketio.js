76d7c08940fadd0a38195b72f545bfb0185874a0
Daniel Dyla <daniel.dyla@dynatrace.com>
2017-11-07 08:51:07 -0500

    Implement socket.io panic behavior

--- src/server/socketio.js
+++ src/server/socketio.js
@@ -3,6 +3,7 @@
 const ClassroomModel = require("./models/classrooms");
 
 const panicked = {};
+const timers = {};
 
 module.exports = (app, io) => {
   io.on("connection", async (socket) => {
@@ -21,7 +22,7 @@ module.exports = (app, io) => {
     app.ee.on(`${socket.user.id}:join`, (classroom) => socket.join(classroom));
     app.ee.on(`${socket.user.id}:leave`, (classroom) => socket.leave(classroom));
 
-    socket.on("panic", (event) => {
+    socket.on("panic", async (event) => {
       const classroom = await ClassroomModel.findOne({
         _id: event.classroom,
         students: socket.user.id,
@@ -29,7 +30,42 @@ module.exports = (app, io) => {
 
       if (!classroom) return;
 
-      app.ee.emit("panic", { user: socket.user.id, classroom: event.classroom });
+      if (event.state === null || event.state === undefined) {
+        event.state = true;
+      }
+
+      app.ee.emit("panic", { user: socket.user.id, classroom: event.classroom, state: event.state });
+    });
+  });
+
+  app.ee.on("panic", (event) => {
+    if (!panicked[event.classroom]) {
+      panicked[event.classroom] = new Set();
+    }
+
+    // clear timer
+    if (timers[event.user] && timers[event.user][event.classroom]) {
+      clearTimeout(timers[event.user][event.classroom]);
+    }
+
+    if (event.state) {
+      panicked[event.classroom].add(event.user);
+      // set timer
+      timers[event.user] = timers[event.user] || {};
+      timers[event.user][event.classroom] = setTimeout(() => {
+        app.ee.emit("panic", {
+          classroom: event.classroom,
+          user: event.user,
+          state: false,
+        });
+      }, 1000 * 10);
+    } else {
+      panicked[event.classroom].delete(event.user);
+    }
+
+    io.in(event.classroom).emit("panic_count_change", {
+      classroom: event.classroom,
+      panicNumber: panicked[event.classroom].size,
     });
   });
 };
