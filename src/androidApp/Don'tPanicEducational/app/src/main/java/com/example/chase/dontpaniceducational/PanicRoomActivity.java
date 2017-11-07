d2bb328b1380f2b8445e0a01f61acafdca472cec
Chase699 <chaseehlert123@gmail.com>
2017-11-07 13:08:28 -0500

    Machine Switch

--- src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/PanicRoomActivity.java
+++ src/androidApp/Don'tPanicEducational/app/src/main/java/com/example/chase/dontpaniceducational/PanicRoomActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
@@ -20,31 +21,30 @@ public class PanicRoomActivity extends AppCompatActivity {
     private SharedPreferences mySharedPreferences;
     public static String MY_PREFS = "MY_PREFS";
     int prefMode = JoinClassActivity.MODE_PRIVATE;
-    private String classroom;
+    private String classroom, token;
     private JsonObject jsonObject;
-
-    {
-        try {
-            panicSocket = IO.socket("http://www.panic-button.stream");
-        } catch(URISyntaxException e){}
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_panic_room);
+        {
+            try {
+                panicSocket = IO.socket("http://www.panic-button.stream");
+            } catch(URISyntaxException e){
+                e.printStackTrace();
+            }
+        }
         mySharedPreferences = getSharedPreferences(MY_PREFS, prefMode);
         classroom = mySharedPreferences.getString("classroom", null);
+        token = mySharedPreferences.getString("token", null);
+        token = token.substring(1, token.length() - 1);
+        token = "Bearer ".concat(token);
         numberOfPanicStudents = (TextView) findViewById(R.id.textView_numberOfPanickedStudents);
         jsonObject = new JsonObject();
         panicSocket.on("panic", panicListener);
+        panicSocket.on("connect", connectListener);
         panicSocket.connect();
-    }
-
-    public void panicButtonClick(View view) {
-        jsonObject.addProperty("classroom", classroom);
-        jsonObject.addProperty("state", "true");
-        panicSocket.emit("panic", jsonObject);
     }
 
     private Emitter.Listener panicListener = new Emitter.Listener() {
@@ -67,4 +67,35 @@ public class PanicRoomActivity extends AppCompatActivity {
             });
         }
     };
+
+    private Emitter.Listener connectListener = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            PanicRoomActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+
+                    } catch (JsonIOException e) {
+                        return;
+                    }
+                    Toast.makeText(PanicRoomActivity.this, "Success",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        panicSocket.emit("login", token);
+    }
+
+    public void panicButtonClick(View view) {
+        jsonObject.addProperty("classroom", classroom);
+        jsonObject.addProperty("state", "true");
+        panicSocket.emit("panic", jsonObject);
+        String something = "Chase";
+    }
 }
