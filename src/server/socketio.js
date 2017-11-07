2b8ee6106a4094be6c110b7fbe0712befadd92de
Daniel Dyla <daniel.dyla@dynatrace.com>
2017-11-07 12:10:39 -0500

    Login socket

--- src/server/socketio.js
+++ src/server/socketio.js
@@ -7,34 +7,36 @@ const timers = {};
 
 module.exports = (app, io) => {
   io.on("connection", async (socket) => {
-    // if not logged in, do nothing
-    if (!socket.handshake.session.user) return;
+    socket.once("login", async (token) => {
+      const user = await Users.findOne({ apiToken: token });
+      if (!user return);
+      socket.user = user;
 
-    socket.user = await UserModel.findById(socket.handshake.session.user);
-
-    // join to all classrooms
-    const classrooms = await ClassroomModel.find({
-      students: socket.user.id,
-    });
-    classrooms.forEach((classroom) => socket.join(classroom.id));
-
-    // join/leave classrooms after socket connection
-    app.ee.on(`${socket.user.id}:join`, (classroom) => socket.join(classroom));
-    app.ee.on(`${socket.user.id}:leave`, (classroom) => socket.leave(classroom));
-
-    socket.on("panic", async (event) => {
-      const classroom = await ClassroomModel.findOne({
-        _id: event.classroom,
+      // join to all classrooms
+      const classrooms = await ClassroomModel.find({
         students: socket.user.id,
       });
+      classrooms.forEach((classroom) => socket.join(classroom.id));
 
-      if (!classroom) return;
+      // join/leave classrooms after socket connection
+      app.ee.on(`${socket.user.id}:join`, (classroom) => socket.join(classroom));
+      app.ee.on(`${socket.user.id}:leave`, (classroom) => socket.leave(classroom));
 
-      if (event.state === null || event.state === undefined) {
-        event.state = true;
-      }
+      socket.on("panic", async (event) => {
+        const classroom = await ClassroomModel.findOne({
+          _id: event.classroom,
+          students: socket.user.id,
+        });
 
-      app.ee.emit("panic", { user: socket.user.id, classroom: event.classroom, state: event.state });
+        if (!classroom) return;
+
+        if (event.state === null || event.state === undefined) {
+          event.state = true;
+        }
+
+        app.ee.emit("panic", { user: socket.user.id, classroom: event.classroom, state: event.state });
+      });
+
     });
   });
 
