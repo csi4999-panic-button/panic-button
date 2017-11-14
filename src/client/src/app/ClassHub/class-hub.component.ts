34c6cd3dd1cd8834749450255cbaeddf1d97d955
Daniel Dyla <daniel.dyla@dynatrace.com>
2017-11-14 09:08:54 -0500

    Basic websocket shell

--- src/client/src/app/ClassHub/class-hub.component.ts
+++ src/client/src/app/ClassHub/class-hub.component.ts
@@ -13,48 +13,57 @@ export class ClassHubComponent {
   socket: any;
   private HTTP: HttpClient;
   token: any;
-  panicState: boolean;
+
+  currentClassroom: string;
+
+  panicStates: {
+    [classroom:string]: boolean,
+  };
+
   panicNumbers: {
     [classroomId: string]: number,
   };
-  classString: String;
-  panicNumber: number;
-  
+
   constructor(private http: HttpClient){
-    this.panicState = false;
+    this.panicStates = {};
+    this.panicNumbers = {};
+    this.currentClassroom = '5a045d038e83e6a7a32d634d';
+
     const url = '/api/v1/authenticate'
     http.post(url, {})
-    .subscribe((data) => {
-      this.token = data["token"];
-      this.socket = io();
-      this.socket.on('connect', () => {
-        this.socket.emit('login', this.token);
-      });
+      .subscribe((data) => {
+        this.token = data["token"];
+        this.socket = io();
+        this.socket.on('connect', () => {
+          this.socket.emit('login', this.token);
+        });
 
-      this.socket.on('login_success', function(sucess:boolean){
-        console.log('connected', sucess);
-      });
-
-       // event has the form:
+        this.socket.on('login_success', function(sucess:boolean){
+          console.log('connected', sucess);
+        })
+        // event has the form:
         // {
         //   classroom: String - classroom id receiving the event
         //   panicNumber: number - the number of currently panic'd students
         // }
-      this.socket.on('panic', (event) => {
-        console.log(event);
-        // this.panicNumbers[event.classroom] = this.panicNumber;
-        // this.UpdateView(this.panicNumber);
-      })
-     });
+          .on('panic', (event) => {
+            console.log(event);
+            this.panicNumbers[event.classroom] = event.panicNumber;
+            // this.UpdateView(event.panicNumber);
+          })
+          .on('panic_state_change', (event) => {
+            this.panicStates[event.classroom] = event.state;
+            console.log("panicked", this.panicStates);
+          });
+      });
   }
 
   Panic(){
-    this.socket.emit("panic", { classroom: "59ff6909a2bc9d1b4cd10493", state: true});
-    this.panicState = !this.panicState;
-    console.log(this.panicState);
+    // set the panic state to the opposite of the current saved state
+    this.socket.emit("panic", { classroom: this.currentClassroom, state: !this.panicStates[this.currentClassroom] });
   }
 
   UpdateView(panicNumber: number){
     console.log(panicNumber);
   }
- }
+}
