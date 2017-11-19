2441ee81a25dc45faaef90afa39a11a502870a5a
tcarrio <tom@carrio.me>
2017-11-18 22:39:17 -0500

    Included search route for schools

--- src/server/routes/api/schools/index.js
+++ src/server/routes/api/schools/index.js
@@ -6,32 +6,36 @@ const Schools = require("../../../models/schools");
 // returns a list of all schools
 router.get("/", async (req, res) => {
     const schools = await Schools.find(req.query);
-    res.json(schools);
+    return res.status(200).json(schools);
 });
+
+router.get("/search/:query", async (req, res) => {
+    const schools = await Schools.find({ name: /req.params.query/ });
+    return res.status(200).json(schools);
+});
+
 
 // create a school
 router.post("/", async (req, res) => {
-    if(req.isAuthenticated()){
-        Schools.create({
-            name: req.body.name,
-            address: req.body.address,
-            city: req.body.city,
-            state: req.body.state,
-            country: req.body.country,
-            zip: req.body.zip,
-            domain: req.body.domain
-        }).then( school => {
-            res.json(school);
-        }).catch( err => {
-            res.json({ sucess: false, message: err });
-        });
-    } else {
-        res.status(401).send();
+    if(!req.isAuthenticated()){
+        return res.status(401).send();
     }
+    
+    return Schools.create({
+        name: req.body.name,
+        address: req.body.address,
+        city: req.body.city,
+        state: req.body.state,
+        country: req.body.country,
+        zip: req.body.zip,
+        domain: req.body.domain
+    })
+    .then( school => res.json(school))
+    .catch( err => res.json({ sucess: false, message: err }));
 });
 
 // update a school by schoolId
-router.put("/id/:schoolId", (req, res) => {
+router.put("/:schoolId", (req, res) => {
     if(!req.isAuthenticated()){
         return res.status(401).send();
     }
